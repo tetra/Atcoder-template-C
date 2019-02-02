bbc1f9896228069a1392144d5952c03b368d2f27
Tetorat <>
2019-02-02 10:36:51 +0900

    first commit

---
+++ template.c
@@ -0,0 +1,19 @@
+// Ver19.02
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define INF 1 << 29
+#define LLINF 4545454545454545454
+#define MOD 1000000007
+#define ll long long
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+int upll(const void *a, const void *b) { return *(ll *)a < *(ll *)b ? -1 : *(ll *)a > *(ll *)b ? 1 : 0; }
+int downll(const void *a, const void *b) { return *(ll *)a < *(ll *)b ? 1 : *(ll *)a > *(ll *)b ? -1 : 0; }
+void sortup(ll *a, int n) { qsort(a, n, sizeof(ll), upll); }
+void sortdown(ll *a, int n) { qsort(a, n, sizeof(ll), downll); }
+
+int main()
+{
+  return 0;
+}
